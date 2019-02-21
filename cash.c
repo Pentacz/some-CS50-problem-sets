1e4d62239730c11cfd4e23258e28a79f2da5e75a
Pentacz <Pentacz@users.noreply.github.com>
2019-02-21 17:30:49 +0000

    20190221T173030Z

--- cash.c
+++ cash.c
@@ -1,19 +1,20 @@
+// Asks for change and prints min nbr of coins
 #include <cs50.h>
 #include <stdio.h>
 #include <math.h>
 
 int main(int argc, string argv[])
 {
+    // Gets change owed
     float x;     
     do
     {
         x = get_float("Change owed: ");
     }
     while (x < 0);
+    // Counts quarters, dimes and nickels needed
     int i = round(x * 100);
-    int a = 0;
-    int b = 0;
-    int c = 0;
+    int a = 0, b = 0, c = 0;
     while (i >= 25)
     {
         i = i - 25; a++;
